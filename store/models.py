4813c7cefe4fea4c183c0b5632d4fdc6e6f8d2c1
Otega Otite <otiteotega@gmail.com>
2025-11-11 10:45:18 +0100

    add product and user model to store

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.CharField(max_length=20, primary_key=True) #Id is automatic in django, but we can still define it thus.
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_used = models.DateTimeField(auto=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
